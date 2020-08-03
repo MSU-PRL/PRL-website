edea22c3fa24be0bdeba127716c0fa94b9442a14
MSU PRL <houwat@gmail.com>
2020-08-03 16:08:28 -0400

    adding smooth scroll to side nav

--- Designs & Prototypes/About page - sticky side nav/css-sticky-side-nav code/src/script.js
+++ Designs & Prototypes/About page - sticky side nav/css-sticky-side-nav code/src/script.js
@@ -1,32 +1,47 @@
 (function populateStickySideMenu() {
-  const h2Tags = document.getElementsByTagName('h2')
-  const prlStickyMenu = document.querySelector('.prl-sticky-nav-inner ul')
-  const h2Titles = () => {
-    for (i of h2Tags) {
-      // Create menu links
-      var listItem = document.createElement('li');
-      var div = document.createElement('div');
-      var span = document.createElement('span');
-      var anchor = document.createElement('a');
-      var anchorAttributeName = i.textContent.toLowerCase().replace(/\s+/g, '-')
-      div.textContent = i.textContent
-      div.className = 'text'
-      anchor.appendChild(div)
-      anchor.appendChild(span)
-      anchor.href = '#' + anchorAttributeName
-      listItem.appendChild(anchor)
-      prlStickyMenu.append(listItem)
-  
-      // Create corresponding h2 anchor targets
-      var anchorTarget = document.createElement('a')
-      anchorTarget.id = anchorAttributeName;
-      anchorTarget.name = anchorAttributeName;
-      anchorTarget.textContent = i.textContent
-      oldNode = i.childNodes[0]
-      i.replaceChild(anchorTarget, oldNode)
-    }
+  const headerTags = document.getElementsByTagName('h2');
+  const prlStickyMenu = document.querySelector('.prl-sticky-nav-inner ul');
+    
+  const createMenuAnchors = () => {
+    for (i of headerTags) {
+    // Create menu links based on h2 tags in document
+    var listItem = document.createElement('li');
+    var div = document.createElement('div');
+    var span = document.createElement('span');
+    var anchor = document.createElement('a');
+    var anchorAttributeName = i.textContent.toLowerCase().replace(/\s+/g, '-')
+    div.textContent = i.textContent
+    div.className = 'text'
+    anchor.appendChild(div)
+    anchor.appendChild(span)
+    anchor.addEventListener('click', menuLinkClick);
+    anchor.href = '#' + anchorAttributeName
+    listItem.appendChild(anchor)
+    prlStickyMenu.append(listItem)
+
+    // Create corresponding h2 anchor targets in document body
+    var anchorTarget = document.createElement('a')
+    anchorTarget.id = anchorAttributeName;
+    anchorTarget.name = anchorAttributeName;
+    anchorTarget.textContent = i.textContent
+    oldNode = i.childNodes[0]
+    i.replaceChild(anchorTarget, oldNode)
   }
-  return h2Titles()
+};
+  
+const menuLinkClick = (event) => {
+  smoothScroll(event) // Use smooth scroll function
+} 
+
+const smoothScroll = (event) => {
+  event.preventDefault();
+  const targetId = event.currentTarget.getAttribute("href");
+  const 
+  console.log(targetId)
+} 
+
+  return createMenuAnchors()
+  
 }());
 
 
@@ -34,6 +49,7 @@
 // Todo:
 // DONE Add <h2> with anchors
 // DONE Style menu
-// DONE JS autopopulate menu feature
+// DONE autopopulate menu
+// DONE <h2> autopopulate with anchor tag
 // Scroll between sections
-// NEW FORK: Tentative: inject aside with JS, inject anchors by scrubbing <h2> text nodes
+// NEW FORK: Tentative: inject aside with JS, add hover when reaching component location
