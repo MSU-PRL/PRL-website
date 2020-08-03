285c955d50cc584b7149a86a4d80f10815c45b7f
Igor Houwat <58915539+ihouwat@users.noreply.github.com>
2020-08-03 19:02:52 -0400

    finished side sticky nav

--- Designs & Prototypes/About page - sticky side nav/css-sticky-side-nav code/src/script.js
+++ Designs & Prototypes/About page - sticky side nav/css-sticky-side-nav code/src/script.js
@@ -1,8 +1,11 @@
 (function populateStickySideMenu() {
-  const headerTags = document.getElementsByTagName('h2');
-  const prlStickyMenu = document.querySelector('.prl-sticky-nav-inner ul');
+  var headerTags = document.getElementsByTagName('h2');
+  var prlStickyMenuInner = document.querySelector('.prl-sticky-nav-inner');
+  var ul = document.createElement('ul')
+  prlStickyMenuInner.appendChild(ul)
+  var prlStickyMenu = document.querySelector('.prl-sticky-nav-inner ul');
     
-  const createMenuAnchors = () => {
+  var createMenuAnchors = () => {
     for (i of headerTags) {
     // Create menu links based on h2 tags in document
     var listItem = document.createElement('li');
@@ -29,20 +32,68 @@
   }
 };
   
-const menuLinkClick = (event) => {
+var menuLinkClick = (event) => {
   smoothScroll(event) // Use smooth scroll function
 } 
 
-const smoothScroll = (event) => {
+var smoothScroll = (event) => {
   event.preventDefault();
-  const targetId = event.currentTarget.getAttribute("href");
-  const 
-  console.log(targetId)
-} 
+  var targetId = event.currentTarget.getAttribute("href");
+  var targetPosition = document.querySelector(targetId).offsetTop;
+  var startPosition = window.pageYOffset;
+  var distance = targetPosition - startPosition;
+  var duration = 1000;
+  let start = null;
 
-  return createMenuAnchors()
+  var scrollAnimation = (timestamp) => {
+    if (!start) {
+      start = timestamp
+    };
+    var progress = timestamp - start;
+    window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
+    if (progress < duration) window.requestAnimationFrame(scrollAnimation);
+  }
+
+
+  var easeInOutCubic = (t, b, c, d) => {
+    t /= d/2;
+    if (t < 1) return c/2*t*t*t + b;
+    t -= 2;
+    return c/2*(t*t*t + 2) + b;
+  };
   
+  return window.requestAnimationFrame(scrollAnimation);
+
+} 
+  
+  window.onscroll = function(){appearUnderHeroImage()}
+  var appearUnderHeroImage = () => {
+    var textStartPosition = document.querySelector('article').firstElementChild.offsetTop;
+    var windowCoords = document.body.getBoundingClientRect().top;
+    var aside = document.getElementById('#prl-sticky-nav')
+    var asideHelper = document.body.querySelector('.aside-helper');
+
+    if (Math.abs(windowCoords) > textStartPosition - 200){
+      aside.classList.add('aside-visible'); 
+      if(window.sessionStorage.getItem('first view') === null) {
+        window.sessionStorage.setItem('first view', true) 
+        asideHelper.style.animationName = 'aside-helper-disappear' // hover helper text appears once per session
+      }
+      else if (window.sessionStorage.getItem('first view') === true) {
+        asideHelper.removeAttribute("style");
+      }
+    }
+    else if (Math.abs(windowCoords) < textStartPosition - 200) {
+      aside.classList.remove('aside-visible')
+      aside.removeAttribute("class")
+    }
+  }
+
+  return createMenuAnchors(), appearUnderHeroImage()
+
 }());
+
+
 
 
   
@@ -51,5 +102,5 @@ const smoothScroll = (event) => {
 // DONE Style menu
 // DONE autopopulate menu
 // DONE <h2> autopopulate with anchor tag
-// Scroll between sections
-// NEW FORK: Tentative: inject aside with JS, add hover when reaching component location
+// DONE Scroll between sections
+// DONE Inject aside when reaching top of body content
