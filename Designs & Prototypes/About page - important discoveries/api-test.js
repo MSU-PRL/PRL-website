11b7dd8bf0cc5ccac00fe629a1d6bd0e01189578
MSU PRL <houwat@gmail.com>
2020-08-31 15:49:46 -0400

    updated api Sciwheel test file

--- Designs & Prototypes/About page - important discoveries/api-test.js
+++ Designs & Prototypes/About page - important discoveries/api-test.js
@@ -2,15 +2,15 @@ const fetch = require("node-fetch");
 
 // Try importing project, sorting by tags, then for each reference, getting comments/notes
 
-const fetchAPI = () =>{
-  fetch('https://sciwheel.com/extapi/work/references/notes', {
+const fetchSciwheelAPI = () =>{
+  fetch('https://sciwheel.com/extapi/work/references/', {
   method: 'Get',
   headers: {'Authorization': 'Bearer 3E6D88C2CB9D47494DE94FFF891FB9E7',
   }
 })
   .then(res => res.json())
-  .then(data => console.log(data))
+  .then(data => console.log(data.results[1]))
   .catch(err => console.log(err))
 }
 
-fetchAPI()
+fetchSciwheelAPI()
